fc6fe88e445f300e7a1f7e40d09f8947a34100a8
DevriesL <therkduan@gmail.com>
2019-08-25 23:11:33 +0800

    update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-#!/bin/bash
-
-#export ARCH=arm64
-#export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
-#export KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
-
 mkdir out
 
-BUILD_CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/6.0/bin/clang
-#BUILD_CROSS_COMPILE=$BUILD_TOP_DIR/PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-#KERNEL_LLVM_BIN=$BUILD_TOP_DIR/toolchain/llvm-arm-toolchain-ship/6.0/bin/clang
+BUILD_CROSS_COMPILE=/home/devries/Work_Space/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+KERNEL_LLVM_BIN=/home/devries/Work_Space/toolchains/snapdragon-llvm-6.0.9-linux64/toolchains/llvm-Snapdragon_LLVM_for_Android_6.0/prebuilt/linux-x86_64/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/scripts/dtc/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y ANDROID_VERSION=90000"
 
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE beyond2qlte_chn_open_defconfig
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
  
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+cp out/arch/arm64/boot/Image-dtb $(pwd)/boot.img-zImage
